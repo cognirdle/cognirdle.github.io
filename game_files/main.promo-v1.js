57913c2e3efec037646a4cec03282fa40d38324c
Greg Brooks <greg.brooks@cognira.com>
2022-05-01 10:01:48 -0400

    google analytics

--- game_files/main.promo-v1.js
+++ game_files/main.promo-v1.js
@@ -1351,7 +1351,7 @@
     customElements.define("game-toast", Aa),
       (window.dataLayer = window.dataLayer || []),
       Ca("js", new Date());
-    Ca("config", "G-51PE0HTHXT", {
+    Ca("config", "G-FYHVYKZX9E", {
       app_version:
         null === (Ea = window.cognirdle) || void 0 === Ea ? void 0 : Ea.hash,
       debug_mode: !1,
